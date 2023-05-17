9cff21d26f407042c92f2cd76b82bd42fdf9fe67
frankwang <jiangda2@illinois.edu>
2023-05-17 14:18:10 -0500

    finish trees

--- tree_hw/moretrees.ml
+++ tree_hw/moretrees.ml
@@ -21,19 +21,31 @@ let rec replace_loki_at_leaf t lokis_replacement=
   ;; *)
 
 
-(**more work*)
+(**more work: dfs or bfs*)
 let has_path t word = 
-  let rec get_rest = function 
-  | (Leaf a) -> a
-  | Tree (v, trees) -> 
-    Core.List.map ~f:(fun tree -> Core.List.fold tree ~f:(fun acc tree -> acc ^ (get_rest tree)) ~init:v) trees
-  in
+  let rec rec_search acc = function 
+  | Leaf x -> (acc ^ x) = word
+  | Tree (v, trees) ->
+    let concat_ = acc ^ v in 
+    (concat_ = word) || (Core.List.fold trees ~f:(fun acc x -> (acc || (rec_search concat_ x))) ~init:false)
+  in rec_search "" t 
 
 
 
 let () = 
+  let open Core in
   let t = Tree ("odin", [Tree ("balder", [Leaf "loki"; Leaf "freya"]); Tree ("frigg", [Leaf "loki"]); Tree ("loki", [Leaf "sif"; Leaf "loki"]); Leaf "loki"]) in 
   Tree.print_tree t;
-  print_endline "";
   let modified_t = replace_loki_at_leaf t "lokis" in 
   Tree.print_tree modified_t;
+  let greeting = Tree ("h", [Leaf "i"; 
+                            Tree ("e", [Tree ("l", [Tree ("l", [Leaf "o"])]);
+                                        Leaf "y"])]) in 
+  print_tree greeting;
+  printf "has h: %b\n" (has_path greeting "h");
+  printf "has i: %b\n" (has_path greeting "i");
+  printf "has hi: %b\n" (has_path greeting "hi");
+  printf "has hey: %b\n" (has_path greeting "hey");
+  printf "has hello: %b\n" (has_path greeting "hello");
+  printf "has bye: %b\n" (has_path greeting "bye");
+  printf "has hint: %b\n" (has_path greeting "hint");
